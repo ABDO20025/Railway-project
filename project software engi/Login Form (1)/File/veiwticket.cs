dce39dbcad10bd095fa97a28abeb47120f24d49d
Abdelrahman yahia <83445402+ABDO20025@users.noreply.github.com>
2022-12-07 04:40:55 +0200

    .

---
+++ project software engi/Login Form (1)/File/veiwticket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Form
+{
+    public partial class veiwticket : Form
+    {
+        public veiwticket()
+        {
+            InitializeComponent();
+        }
+
+        private void veiwticket_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
