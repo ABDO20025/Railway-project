3904267c0cb929c78808fcf8d7a2f22b01ddac64
Abdelrahman yahia <83445402+ABDO20025@users.noreply.github.com>
2022-12-07 02:41:05 +0200

    .

---
+++ project software engi/Login Form (1)/File/admin.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Login_Form
+{
+    partial class admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Plum;
+            this.ClientSize = new System.Drawing.Size(978, 599);
+            this.Name = "admin";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.admin_FormClosed);
+            this.Load += new System.EventHandler(this.admin_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
